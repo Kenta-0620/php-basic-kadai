23dc64dc0bc40cb9ee0e1b6440fff2c39ac65b38
Kenta-0620 <k.nagano0620@gmail.com>
2024-10-08 19:29:51 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+    <p>
+        <?php
+        $vegetable_data = ['name' => 'onion', 'price' => 200, 'weght' => 160];
+
+        print_r($vegetable_data);
+
+        ?>
+    </p>
+ </body>
+ 
+ </html>
